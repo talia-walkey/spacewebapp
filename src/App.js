65d72aaa31382030135d73a0a84d1ad17d720a9c
Talia Walkey <a00992777@se14l0124-11.ad.bcit.ca>
2018-02-06 17:14:21 -0800

    added new files

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import './App.css';
+import Chat from './comp/chat.js';
+import WebFont from 'webfontloader';
+import mySocket from ‘socket.io-client’;
+
+WebFont.load({
+  google: {
+    families: ['Titillium Web:300,400,700', 'sans-serif']
+  }
+});
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state={
+           tab:0 
+        };
+        
+        this.startChat=this.startChat.bind(this);
+    }
+    
+    startChat(){
+        this.setState({
+            tab:1
+        })
+    }
+  render() {
+      var comp=null;
+      if(this.state.tab ===0){
+          comp=(
+          <div>
+              <button id="chatButton" onClick={this.startChat}>Lets Chat!!</button>
+            <div id="topPage">
+                <img src={require('./Image/background.svg')} id="bkgImg" />
+                <div id="welcome" class="nameHeading" id="welcomeDiv">Welcome to our landing page!</div>
+            </div>
+
+            <div id="images">
+                <img src={require('./Image/AlynnaImg.png')} id="aimg" />
+                <img src={require('./Image/TaliaImg.png')} id="timg" />
+            </div>
+
+        <div id="tbioBkg">
+                I am a marker, developer, and designer with a Digital Design and Development Diploma from British Columbia Institute of Technology! I have skills in everything from design to development of web and mobile applications as well as I have learned the business skills to market them. 
+        </div>
+                <h1 id="tname">Talia Walkey</h1>
+                <img src={require('./Image/speechleft.png')} id="speechleft" />
+        <br/>
+        <div id="abioBkg">
+                Hello! I am an aspiring UX/UI designer currently studying Digital Design and Development at BCIT. I am a creative and quality driven individual, motivated to create new user experiences. I love to bring my ideas to life, either through graphics and/or websites.
+        </div>
+                <h1 id="aname">Alynna Alcira</h1>
+
+                <img src={require('./Image/speechright.png')} id="speechright" />
+                
+          </div>
+        
+      )
+      }else if(comp=1){
+          comp=(
+          <div>
+            <div id="topPage">
+                <img src={require('./Image/background.svg')} id="bkgImg" />
+                <div id="welcome" class="nameHeading" id="welcomeDiv">Time to Chat!</div>
+            </div>
+                    
+        <div id="chatDiv">
+            <Chat/>
+        </div>
+                
+          </div>
+        
+      )
+      }
+    return (
+      <div className="App">
+            {comp}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/comp/chat.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import mySocket from 'socket.io-client';
+
+class Chat extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state={
+            myname:"",
+            mode:0,
+            allnames:[],
+            allmsgs:[],
+            mymsg:""
+        }
+        
+        this.joinChat=this.joinChat.bind(this);
+        this.handleName=this.handleName.bind(this);
+        this.handleMyMsg=this.handleMyMsg.bind(this);
+        this.sendMsg=this.sendMsg.bind(this);
+    }
+    
+    componentDidMount(){
+        //this.socket =mySocket("http://localhost:10001");
+    }
+    
+    joinChat(){
+        this.setState({
+            mode:1
+        })
+        this.socket =mySocket("http://localhost:10001");
+            //send the username entered in the input to the socket server
+        this.socket.emit("uname", this.state.myname);
+        this.socket.on("names", (data)=>{
+            this.setState({
+                allnames:data
+            });
+        });
+        this.socket.on("msgs",(data)=>{
+           this.setState({
+               allmsgs:data
+           }); 
+        });
+    }
+    
+    handleName(evt){
+        this.setState({
+            myname:evt.target.value
+        })    
+    }
+    
+    handleMyMsg(evt){
+        this.setState({
+            mymsg:evt.target.value
+        })
+    }
+    sendMsg(){
+        var msg = this.state.myname+": "+this.state.mymsg;
+        //Henry: Hi
+        
+        this.socket.emit("sendmsg", msg);
+    }
+    
+  render() {
+      var comp = null;
+      if(this.state.mode===0){
+          comp = (
+                <div class="chatBox">
+                    <input 
+                        onChange={this.handleName}
+                        type="text" 
+                        placeholder="type in your username"
+                    />
+                    <button onClick={this.joinChat}>
+                        Join Chat
+                    </button>
+                </div>
+          );
+      }else if(this.state.mode===1){
+          
+          var allmsgs = this.state.allmsgs.map((obj,i)=>{
+             return(
+                <div key={i}>
+                 {obj}
+                </div>
+             ) 
+          })
+          
+          comp=(
+                <div class="chatBox">
+                    <div id="chatDisplay">{allmsgs}</div>
+                    <div id="controls">
+                        <input 
+                            type="text" 
+                            placeholder="type your message here" 
+                            onChange={this.handleMyMsg}/>
+                        <button 
+                            onClick={this.sendMsg}
+                        >Send</button>
+                    </div>
+                </div>
+          );
+      }
+      
+      var allnames = this.state.allnames.map((obj, i)=>{
+          return(
+                <div key={i}>
+                    {obj}
+                </div>
+          )
+      })
+      
+    return (
+        <div className="App">
+            {comp}
+            <div id="allNames">
+                People who are online <hr/> 
+                {allnames}
+            </div>
+        
+        </div>
+    );
+  }
+}
+
+export default Chat;
